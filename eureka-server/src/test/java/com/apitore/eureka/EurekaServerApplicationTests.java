26a140baae3e31e8ed70b492c4bd01f7bf63f60a
keigo.hattori <keigoht@gmail.com>
2016-03-20 00:26:05 +0900

    eureka server sample commit

---
+++ eureka-server/src/test/java/com/apitore/eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,18 @@
+package com.apitore.eureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.apitore.eureka.EurekaServerMain;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = EurekaServerMain.class)
+public class EurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
